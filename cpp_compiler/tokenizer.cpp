11472d63b1d609e704c4f567adc0acdbd7ff5f41
Milosz Tanski <mtanski@gmail.com>
2010-11-24 01:42:45 -0500

    More tokenizer code.

--- cpp_compiler/tokenizer.cpp
+++ cpp_compiler/tokenizer.cpp
@@ -1,6 +1,30 @@
 #include "tokenizer.hpp"
 
 
+enum digit_base { BASE_16, BASE_10, BASE_8, BASE_2 }; 
+
+static inline bool isoctal(char c)
+{
+  switch (c) {
+    case '0' ... '7': return true;
+  }
+
+  return false;
+}
+
+static inline bool isbase(digit_base base, char c)
+{
+  switch (base) {
+    BASE16: return isxdigit(c);
+    BASE10: return isdigit(c);
+    BASE8:  return isoctal(c);
+    BASE2:  return (c == '0' || c == 1);
+  }
+
+  return false;
+}
+
+
 /* Prototype for token functions */
 typedef size_t(*token_func)(const char *input_str);
 
@@ -12,9 +36,22 @@ static size_t is_##name(const char *input_str)  \
   return (*input_str == c);                     \
 }
 
+/* Get a back betweeb start and end */
+#define TOKEN_BLOCK(name, s, e)                 \
+static size_t is_##name(const char *input_str)  \
+{                                               \
+  if (strcmp(input_str, s) == 0) {              \
+    const char *end = strstr(input_str, e);     \
+                                                \
+    if (end != NULL)                            \
+      return end - input_str;                   \
+  }                                             \
+  return 0;                                     \
+}
+
 
 /* Simple tokenizers */
-TOKEN_CHAR(sep_stement,   ';')
+TOKEN_CHAR(sep_statement, ';')
 TOKEN_CHAR(sep_list,      ',')
 TOKEN_CHAR(sep_type,      ':')
 TOKEN_CHAR(sep_member,    '.')
@@ -25,26 +62,17 @@ TOKEN_CHAR(r_bracket,     ']')
 TOKEN_CHAR(l_curly,       '{')
 TOKEN_CHAR(r_curly,       '}')
 
+/* Simple blocks */
+TOKEN_BLOCK(comment_long,  "/*", "*/")
+TOKEN_BLOCK(value_string,  "\"", "\"")
+TOKEN_BLOCK(value_char,     "'", "'")
+
 
 static
 size_t is_comment_line(const char* input_str)
 {
   if (strcmp(input_str, "//") == 0) {
     return strcspn(input_str, "\r\n");
-  }
-
-  return 0;
-}
-
-static
-size_t is_comment_long(const char* input_str)
-{
-  if (strcmp(input_str, "/*") == 0) {
-    const char *end = strstr(input_str, "*/");
-
-    if (end != NULL) {
-      return end - input_str;
-    }
   }
 
   return 0;
@@ -71,17 +99,57 @@ size_t is_whitespace(const char* input_str)
 }
 
 static
-size_t is_value_string(const char* input_str)
+size_t is_value_float(const char* input_str)
 {
-  if (*input_str == '"') {
-    const char *end = strchr(input_str + 1, '"');
+  const char *end = input_str;
 
-    if (end != NULL) {
-      return end - input_str;
-    }
+  if (*end == '-')
+    end++;
+
+  for (; *end != '\0'; end++) {
+    if (!isdigit(*end)) break;
   }
 
-  return 0;
+  if (*end != '.') {
+    return 0;
+  }
+
+  for (; end != '\0'; end++) {
+    if (!isdigit(*end)) break;
+  }
+
+  return (*end != '.') ? end - input_str : 0;
+}
+
+
+static
+size_t is_value_number(const char* input_str)
+{
+  const char *end = input_str;
+  digit_base  base = BASE_10;
+
+  if (*end == '-')
+    end++;
+
+  if (*end == '0') {
+    switch (end[1]) {
+      case 'x': base = BASE_16; break;
+      case 'o': base = BASE_8;  break;
+      case 'b': base = BASE_2;  break;
+      default:
+        return 0;
+    }
+    end += 2;
+  }
+
+  if (!isbase(base, *end))
+    return 0;
+
+  for (end++; *end != '\0'; end++) {
+    if (isbase(base, *end)) break;
+  }
+
+  return end - input_str;
 }
 
 static
@@ -108,11 +176,11 @@ token_func token_functions[] = {
   is_sep_type,
   is_sep_member,
   is_value_string,
-  is_value_number,
   is_value_float,
+  is_value_number,
   is_value_char,
   is_keyword,
-}
+};
 
 
 token_list tokenizer(const char *input_str)
