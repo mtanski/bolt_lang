976d9113351bedfa1b2d488c1cb0996318068b74
Milosz Tanski <mtanski@gmail.com>
2010-12-04 16:57:13 -0500

    Get the error in the line.

--- cpp_compiler/utils.h
+++ cpp_compiler/utils.h
@@ -1,4 +1,18 @@
+#ifndef __UTILS_H__
+#define __UTILS_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 /* stringify macro */
 #define __STRINGIFY(str) #str
 #define STRINGIFY(str) __STRINGIFY(str)
+
+const char* get_line(const char *start, size_t len,  const char *pos);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __UTILS_H__ */
